f4f3a0ed7f97502440f33012bd814ed3320a85d0
NielsLauritsMeldgaard <62181856+NielsLauritsMeldgaard@users.noreply.github.com>
2024-11-07 21:12:38 +0100

    Setting up the project

---
+++ inc/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+// Include necessary standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function declarations
+#define MAX 100
+
+#endif // UTILS_H
